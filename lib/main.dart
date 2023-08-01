1595cde16146934e19dd95881e634665fb7cbaf1
lee <t19960804@gmail.com>
2023-08-01 11:54:04 +0800

    Add TextButton / Selection Logic

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,12 @@ class CalendarPage extends StatefulWidget {
 class _CalendarPageState extends State<CalendarPage> {
   var _bottomNavigationIndex = 0;
   var _isEditMode = false;
+  var _trainings = ["Incline bench press", "Decline bench press"];
+  var _selectStatus = [];
+
+  _CalendarPageState() {
+    _selectStatus = List.generate(_trainings.length, (index) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -102,46 +108,73 @@ class _CalendarPageState extends State<CalendarPage> {
               child: ListView.builder(
                 // 在Column裡面使用ListView這種具有無限延展性的Widget，需要用Expanded包住
                 itemBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.all(10),
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Theme.of(context).colorScheme.primary,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
+                  final training = _trainings[index];
+                  final isSelected = _selectStatus[index];
+
+                  return TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectStatus.fillRange(0, _selectStatus.length, false);
+                        _selectStatus[index] = true;
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      height: 80,
+                      decoration: BoxDecoration(
+                        border: isSelected
+                            ? null
+                            : Border.all(
+                                color: Colors.grey,
+                                width: 2.0,
+                              ),
+                        borderRadius: BorderRadius.circular(10),
+                        color: isSelected
+                            ? Theme.of(context).colorScheme.primary
+                            : Colors.transparent,
+                        boxShadow: isSelected
+                            ? [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ]
+                            : [],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 12, right: 12),
+                        child: Row(
+                          children: [
+                            Text(
+                              training,
+                              style: TextStyle(
+                                fontWeight: isSelected
+                                    ? FontWeight.bold
+                                    : FontWeight.normal,
+                                fontSize: 24,
+                                color: isSelected ? Colors.black : Colors.grey,
+                              ),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "2K",
+                              style: TextStyle(
+                                fontWeight: isSelected
+                                    ? FontWeight.bold
+                                    : FontWeight.normal,
+                                fontSize: 24,
+                                color: isSelected ? Colors.black : Colors.grey,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.only(left: 12, right: 12),
-                      child: Row(
-                        children: [
-                          Text(
-                            "Incline Bench Press",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                            ),
-                          ),
-                          Spacer(),
-                          Text(
-                            "2K",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                            ),
-                          ),
-                        ],
                       ),
                     ),
                   );
                 },
-                itemCount: 5,
+                itemCount: _trainings.length,
               ),
             )
           ],
