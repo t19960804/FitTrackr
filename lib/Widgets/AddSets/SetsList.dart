68686d6b70b6746ca8bb6fc6f5b741237045e14c
lee <t19960804@gmail.com>
2023-08-14 12:20:43 +0800

    Add ShakeAnimationWidget To Refactor

--- lib/Widgets/AddSets/SetsList.dart
+++ lib/Widgets/AddSets/SetsList.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fit_trackr/Models/TrainingSet.dart';
 import 'package:fit_trackr/Widgets/DeleteButton.dart';
-import 'package:fit_trackr/Helpers/ShakeAnimationHelper.dart';
+import 'package:fit_trackr/Widgets/Animation/ShakeAnimationWidget.dart';
 
 class SetsList extends StatefulWidget {
   final bool isEditMode;
@@ -19,127 +19,96 @@ class SetsList extends StatefulWidget {
 }
 
 class _SetsListState extends State<SetsList> with TickerProviderStateMixin {
-  late ShakeAnimationHelper helper;
-
-  @override
-  void initState() {
-    super.initState();
-    helper = ShakeAnimationHelper(provider: this);
-    helper.stop();
-  }
-
-  @override
-  void didUpdateWidget(SetsList oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.isEditMode == true) {
-      helper.start();
-    } else {
-      helper.stop();
-    }
-  }
-
-  @override
-  void dispose() {
-    helper.controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         final trainingSet = widget.sets[index];
-        return AnimatedBuilder(
-          animation: helper.animation,
-          builder: (BuildContext context, Widget? child) {
-            return Transform.rotate(
-              angle: helper.getShakingAngle(widget.isEditMode),
-              child: Stack(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(20),
-                    height: 120,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 3.0,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.transparent,
+        return ShakeAnimationWidget(
+            isShaking: widget.isEditMode,
+            child: Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  height: 120,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 3.0,
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            _toOrdinal(index + 1),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.transparent,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          _toOrdinal(index + 1),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "${trainingSet.reps}",
                             style: const TextStyle(
                               fontWeight: FontWeight.bold,
-                              fontSize: 24,
+                              fontSize: 28,
                               color: Colors.grey,
                             ),
                           ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text(
-                              "${trainingSet.reps}",
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 28,
-                                color: Colors.grey,
-                              ),
+                          const Text(
+                            "reps",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18,
+                              color: Colors.grey,
                             ),
-                            const Text(
-                              "reps",
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 18,
-                                color: Colors.grey,
-                              ),
+                          ),
+                          Container(
+                            width: 40,
+                          ),
+                          Text(
+                            "${trainingSet.kg}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28,
+                              color: Colors.grey,
                             ),
-                            Container(
-                              width: 40,
+                          ),
+                          const Text(
+                            "kg",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18,
+                              color: Colors.grey,
                             ),
-                            Text(
-                              "${trainingSet.kg}",
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 28,
-                                color: Colors.grey,
-                              ),
-                            ),
-                            const Text(
-                              "kg",
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 18,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  Positioned(
-                    right: 5,
-                    top: 5,
-                    child: Visibility(
-                      visible: widget.isEditMode,
-                      child: DeleteButton(onTap: () {
-                        widget.deleteButtonTapped(widget.sets[index]);
-                      }),
-                    ),
+                ),
+                Positioned(
+                  right: 5,
+                  top: 5,
+                  child: Visibility(
+                    visible: widget.isEditMode,
+                    child: DeleteButton(onTap: () {
+                      widget.deleteButtonTapped(widget.sets[index]);
+                    }),
                   ),
-                ],
-              ),
-            );
-          },
-        );
+                ),
+              ],
+            ));
       },
       itemCount: widget.sets.length,
     );
---
+++ lib/Widgets/Animation/ShakeAnimationWidget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:fit_trackr/Helpers/ShakeAnimationHelper.dart';
+
+class ShakeAnimationWidget extends StatefulWidget {
+  final bool isShaking;
+  final Widget child;
+  const ShakeAnimationWidget(
+      {super.key, required this.isShaking, required this.child});
+
+  @override
+  State<ShakeAnimationWidget> createState() => _ShakeAnimationWidgetState();
+}
+
+class _ShakeAnimationWidgetState extends State<ShakeAnimationWidget>
+    with TickerProviderStateMixin {
+  late ShakeAnimationHelper helper;
+
+  @override
+  void initState() {
+    super.initState();
+    helper = ShakeAnimationHelper(provider: this);
+    _checkIfDoShakeAnimation();
+  }
+
+  @override
+  void didUpdateWidget(ShakeAnimationWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _checkIfDoShakeAnimation();
+  }
+
+  void _checkIfDoShakeAnimation() {
+    if (widget.isShaking == true) {
+      helper.start();
+    } else {
+      helper.stop();
+    }
+  }
+
+  @override
+  void dispose() {
+    helper.controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: helper.animation,
+      builder: (BuildContext context, Widget? child) {
+        return Transform.rotate(
+          angle: helper.getShakingAngle(widget.isShaking),
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
